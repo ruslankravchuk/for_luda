f00046c338c5421c40dd65d7b6cb165a82d900cd
Ruslan <ruslan.kravchuk79@gmail.com>
2022-02-14 21:32:38 +0200

    main(): general commit

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,6 @@ var gulp        = require('gulp'), // Подключаем Gulp
     browserSync = require('browser-sync'), // Подключаем Browser Sync
     sassGlob = require('gulp-sass-glob'),
     smartgrid   = require('smart-grid');
-    
-
-    
-// gulp.task('less2sass', function(){ // Создаем таск Sass
-//     return gulp.src('less/**/*.less') // Берем источник
-//         .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
-//         .pipe(gulp.dest('sass')) // Выгружаем результата в папку app/css
-//         .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
-// });
-
 
 
 gulp.task('sass', function(){ // Создаем таск Sass
@@ -22,21 +12,18 @@ gulp.task('sass', function(){ // Создаем таск Sass
         .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
 });
 
-
-
 gulp.task('styles', function () {
     return gulp.src('scss/*.scss')
         .pipe(sassGlob())
         .pipe(sass())
-        .pipe(gulp.dest('sass'));
+        .pipe(gulp.dest('dest/css'));
 });
 
 gulp.task('browser-sync', function() { // Создаем таск browser-sync
     browserSync({ // Выполняем browserSync
         server: { // Определяем параметры сервера
             baseDir: './' // Директория для сервера - app
-        }
-        
+        }        
     });
 });
 
@@ -52,50 +39,6 @@ gulp.task('script', function() {
 
 });
 
-/* It's principal settings in smart grid project */
-var settings = {
-    outputStyle: 'sass', /* less || scss || sass || styl */
-    columns: 12, /* number of grid columns */
-    offset: '30px', /* gutter width px || % || rem */
-    mobileFirst: false, /* mobileFirst ? 'min-width' : 'max-width' */
-    container: {
-        maxWidth: '1200px', /* max-width оn very large screen */
-        fields: '30px' /* side fields */
-    },
-    breakPoints: {
-        lg: {
-            width: '1100px', /* -> @media (max-width: 1100px) */
-        },
-        md: {
-            width: '960px'
-        },
-        sm: {
-            width: '780px',
-            fields: '15px' /* set fields only if you want to change container.fields */
-        },
-        xs: {
-            width: '560px'
-        }
-        /* 
-        We can create any quantity of break points.
-
-        some_name: {
-            width: 'Npx',
-            fields: 'N(px|%|rem)',
-            offset: 'N(px|%|rem)'
-        }
-        */
-    }
-};
-
-gulp.task('smartgrid', function(){
-  return smartgrid('sass/', settings)
-// .pipe(smartgrid())
-// .pipe(gulp.dest('css')) 
-// .pipe(browserSync.reload({stream: true}))
-
-});
-
 gulp.task('jpg', function() {
   return gulp.src(['images/*.jpg', 'images/*.png'])
   .pipe(browserSync.reload({stream: true}))
@@ -108,20 +51,14 @@ gulp.task('css', function() {
 
 });
 
-
-
 gulp.task('watch', function() {
-    gulp.watch('sass/**/*.sass', gulp.parallel('smartgrid'));
     gulp.watch('sass/**/*.sass', gulp.parallel('sass'));     // Наблюдение за sass файлами
     gulp.watch('css/**/*.css', gulp.parallel('css'));
     gulp.watch(['js/**/*.js', 'lib/**/*.js'], gulp.parallel('script'));
     gulp.watch('./index.html', gulp.parallel('kod')); 
     gulp.watch(['images/**/*.jpg', 'images/**/*.png'], gulp.parallel('jpg'));
     
-
 });
-gulp.task('default', gulp.parallel('sass', 'smartgrid', 'browser-sync', 'watch'));
-// gulp.task('mytask', function() {
-//     console.log('Привіт бобер!!!');
-// });
+gulp.task('default', gulp.parallel('sass', 'browser-sync', 'watch'));
+
     
--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-window.onload =  function() {
-	var StyleBack = {'background-color': 'rgba(199, 203, 209, .9)', 
+window.onload =  function() { var StyleBack = {'background-color': 'rgba(199, 203, 209, .9)', 
 	'z-index': '2',
 	'position': 'absolute', 
-	'min-width': '100%',
-	'min-height': '100%',
+	'min-width': '140%',
+	'min-height': '120%',
 	'display': 'flex',                                     
 };
 var hide_smf = {
@@ -15,6 +14,14 @@ var show_smf =   {
 var anime_batom = {"box-shadow": "0 0 10px rgba(0,0,0,0.5)",
 "border-radius": "32px",
 "background-color": "red"};
+var footer_icon = $("footer i");
+var fixet_header = $("header .wraper");
+var width_body = $("body, html").width();
+var height_body = $("body, html").height();
+var Timer;
+
+$(fixet_header).ready(()=>{$(window).scroll()>200 ? $(this).css({display: "none"}) : $(this).show(40)})
+
 
 //---------------------------------АНІМАЦІЯ КНОПКИ ФОРМИ ЗВОРОТНЬОГО ЗВІЗКУ-----------------------//                                              
 $(".form_booton").hover(function () {
@@ -27,51 +34,64 @@ $(".form_booton").hover(function () {
 		, 3000)});
 //--------------------------------вІДКРИВАННЯ ВІКНА ФОРМИ--------------//
 $(".form_booton").click(()=>{
-	var width_wrap = $("body,html").width();
+	
 	var width_form = $(".hiden").width();
 	$(".hiden").css({"display": "flex",
 		"z-index": 3,
-		"right": (width_wrap/2)-(width_form/2)});
+		"right": (width_body/2)-(width_form/2)});
 	$("body").prepend("<div id='wraper_top'></div>");
 	$("#wraper_top").css(StyleBack); 
-
 });
 $(".hiden span").click(()=>{$(".hiden").css("display", "none");
 	$("#wraper_top").remove()
 });
 
+/////------------------------------------КНОПКА МОБІЛЬНОЇ ВЕРСІЇ----------------//////
 
-//---------------------------------------------ЗМІНА ОБЄКТІВ ПО СКРОЛУ-----------------------//
 
-var footer_icon = $("footer i");
-var fixet_header = $("header .wraper");
+$('#Mobile_button').click((e)=>{
+	Open_mobile(e);
+      
+});
 
-// $(window).scroll(
-// 	(e)=>{ 
+function Open_mobile (e) {
+	if ($(e.currentTarget).find('div:nth-child(1)').css("opacity") == 1)
+	           {
+	            $(e.currentTarget).find('div:nth-child(1)').animate({"opacity": '0'}, 1000, 
+				()=>{$(e.currentTarget).find('div:nth-child(1)').css({"display": 'none'});
+					 $(e.currentTarget).find('div:nth-child(2)').animate({"opacity": '1'}, 1000)})
+			   } 
+	       else {
+			    
+			   	$(e.currentTarget).find('div:nth-child(2)').animate({"opacity": '0'}, 1000, 
+			  ()=>{$(e.currentTarget).find('div:nth-child(1)').css({"display": ''}).animate({"opacity": '1'}, 1000, ()=>{  
+				             $(e.currentTarget).find('div').each(
+
+								(i, elem)=> {
+                                       $(elem).removeAttr('style')
+								}
+							 )
+                             
+                    			  }
+					 
+			       )}) 
+                   }
+	               }     
+//---------------------------------------------ЗМІНА ОБЄКТІВ ПО КРОЛУ-----------------------//
+
+$(window).scroll(
+	(e)=>{ 
 		
-// 		$(this).scrollTop() >= 200 
-// 		? $(".botom_on_top").show(500)
+		$(this).scrollTop() >= 200 
+		? $(".botom_on_top").show('slow', ()=>{$(this).removeAttr("style")})
+		: $(".botom_on_top").hide('slow');
 
-// 		: $(".botom_on_top").hide(500);
-
-//      ($(this).scrollTop() <= 260 && $("body, html").width() >= 800 ) 
-// 		? $(fixet_header).fadeIn(1000)  
-
-// 		: $(fixet_header).fadeOut(1000);
-// 		// function () { $(this).removeAttr("style")
-
+		if (width_body > 899)
+        {($(this).scrollTop() <= 50 ) 
+		? $(fixet_header).fadeIn(1000, ()=>{$(this).removeAttr("style")})  
+		: $(fixet_header).fadeOut(1000);}
 		
-//       $(this).scrollTop() >=800
-// 		? 
-// 		($(blockNews1).addClass("animated bounceInLeft"),
-// 			$(blockNews3).addClass("animated bounceInRight"),
-// 			$(blockNews2).addClass("animated pulse"))
-// 		:
-// 		($(blockNews1).removeClass("animated bounceInLeft").removeAttr('style'),
-// 			$(blockNews3).removeClass("animated bounceInRight").removeAttr('style'),
-// 			$(blockNews2).removeClass("animated pulse")).removeAttr('style');
-
-// 	});
+	});
 
 //----------------------АНІМАЦІЯ ПОВІЛЬНОГО ПЕРЕХОДУ ТОП СТОРІНКИ---------------///
 
@@ -83,41 +103,117 @@ $(".botom_on_top").click((e)=> {
 
 //------------------------------ТЕКСТ ДЛЯ БЛОКІВ З НОВИНАМИ--------------////
   
-       $(".block_news").each(function (item, elem)
-              { 
-				$(elem).find("button").click( ()=>{Block_show_next(this)});	                   
-				});
-	          
-	function Block_show_next (e) {
-           $("html, body").width() >= 1300 
-		 ? ( width_page = "-26em" )  : (width_page = "0em");
-            $(e).find("p").filter(":last").css({display: "flex"});
-		   
-		 { 
-			//  .find("p").filter(":last").css("backgroundcolor", "black")
-        // $(this).parent().css({marginBottom: width_page}).animate({minHeight: "43em"}, 500); .css("display")
-		// 	$(this).prev().find("p").filter(":last")
-		// 	.css({ 'display': 'flex'}).animate({opacity: 1, 
-		// 	                                     minHeight: "20em" }, 1000, function() {
-		// 	                                     });
-		// 	$(this).children().filter("span").text("Меньше...");
+       $(".block_news").each(function (i ,elem)
+                 { 
+				$(elem).find("button").click( ()=>{Block_show_next(elem)} );
+                });				 
 
-      	//  }        
-			// else 	{
-			// 	var uuuu = height_block_news.toString();
-			// 	$(this).parent().animate({minHeight: uuuu}, 1000,  function () {
-			// 		$(this).removeAttr('style')});
-			// 	$(this).prev().find("p").filter(":last")
-			// 	.animate({minHeight: "0em", 
-			// 		opacity: 0.1}, 1000, function () {
-						
-			// 			$(this).removeAttr('style');
-			// 							}
-			// 		);
-			//    $(this).children().filter("span").text("Більше...");
-			   
-			 }
+				// let $size_p_news_block = 420;
+       	  
+	        function Block_show_next (elem, size_p = 340) {
+				   this.size_p =  size_p;
+                   let $height_block = $(elem).height();
+				   let $height_block_max = $height_block + size_p;
+				   let $width_page = $("html, body").width();
+				   $width_page > 1250 ? $height_block_max += 100 : $height_block_max += 30;
+				   					
+		        if ($height_block < 490)   {
+                    $(elem).animate(({minHeight: String($height_block_max)}), 1000);
+			        $(elem).find("p").filter(":last").toggle(1000, ()=>{ $(elem).find("button span").text("Меньше...")});
+			   			                                 }
+			    else {
+                    $(elem).animate(({minHeight: "400px"}), 1000, ()=>{$(elem).removeAttr("style")} );
+			        $(elem).find("p").filter(":last").toggle(1000, ()=>{ $(elem).find("button span").text("Більше...")})
+					  }
+				}		  
+           
+				
+//----------------------------Animation---------------------------------------------------//
+var NUMBER = 70
+width_body >= 1200 ? true : width_body < 960 ? NUMBER = 15 : NUMBER = 35
+const SPEED = 5
+const DIST = 200
+const canvas = document.querySelector('.anim_fon')
+const context = canvas.getContext('2d')
+const WIND_WIDTH = document.documentElement.clientWidth
+canvas.width =    WIND_WIDTH
+canvas.height =   height_body
+const points = []
+
+for (let i = 0; i < NUMBER; i++) {
+    const point = {	  
+		x: getRandom(0, canvas.width),
+		y: getRandom(0, canvas.height),
+		angle: getRandom(0, 2 * Math.PI)
+	}	 
+	points.push(point)
+                               }
+  
+	function tick () {
+	    drawBackground()	   
+	    drawPoints()
+		movePoints()
+		drawLine()
+		requestAnimationFrame(tick)		 
+        // return setTimeout(tick, 300)
+                      }
+
+    function getRandom (min, max) {
+	    return min + Math.random() * (max - min + 1)
+                                }
+
+    function drawBackground () {
+	      context.fillStyle = 'black'
+	      context.fillRect(0, 0, WIND_WIDTH, height_body)
+                               }
+    function drawPoints () {		   
+	      for (const point of points) {
+		   context.beginPath()
+		   context.fillStyle = 'yellow'
+		   context.arc(point.x, point.y, 4, 0, Math.PI * 2)
+		   context.fill()
+	                                 }
+						 }
+						 
+    function movePoints()  {
+			for (const point of points)  
+			{     
+				point.x = point.x + SPEED * Math.cos(point.angle)
+				point.y = point.y + SPEED * Math.sin(point.angle) 
+				
+				point.x < 0            ? (point.x = canvas.width + point.x):
+				point.x > canvas.width ? (point.x =   point.x - canvas.width):
+                point.y < 0            ? (point.y = canvas.width + point.y):
+				point.y > canvas.width ? (point.y =   point.y - canvas.width):
+				true
 			}
+						  }
+						  
+	function drawLine () {
+					 for (let i = 0; i < NUMBER - 1; i++) {
+                            for (let j = 0; j < NUMBER; j++) {
+								 const pointA = points[i]
+								 const pointB = points[j]
+								 const dist = getDist(pointA, pointB)
+								
+								if (dist <= DIST) {
+									context.strokeStyle = 'red'
+									context.lineWidth = (1 - dist / DIST)**3
+									context.moveTo(pointA.x, ((pointA.y)-2))
+									context.lineTo(pointB.x, ((pointB.y)-2))
+									context.stroke()
+								                 }
+							              }
+					                }            
+							   }
+	function getDist (a, b) {
+		return ((a.x - b.x)**2 + (a.y - b.y)**2)**0.5
+	                        }
+
+ tick()
+			   
+			 
+			
 
 		
 }
